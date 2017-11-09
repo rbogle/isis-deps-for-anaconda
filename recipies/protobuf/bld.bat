0938831a56d6b5a1fd625400c6f3c4e05b680ed3
rian bogle <rbogle@usgs.gov>
2017-11-09 16:21:38 -0700

    remove mainstream recipies add non-mainstream reciples

--- recipies/protobuf/bld.bat
+++
@@ -1,5 +0,0 @@
-cd python
-if errorlevel 1 exit 1
-
-"%PYTHON%" setup.py install --old-and-unmanageable
-if errorlevel 1 exit 1
--- recipies/protobuf/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd python
-$PYTHON setup.py install --old-and-unmanageable
-
-rm $SP_DIR/*-nspkg.pth
