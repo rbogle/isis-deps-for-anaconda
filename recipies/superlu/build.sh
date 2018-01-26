ea88cb077664bc7eb6d5cd40852a1ab3d9096dcb
Ian Humphrey <ihumphrey@usgs.gov>
2018-01-26 11:50:23 -0700

    enabled shared lib creation and disabled fortran interface for superlu build

--- recipies/superlu/build.sh
+++ recipies/superlu/build.sh
@@ -5,8 +5,14 @@ cd cbuild
 
 BUILD_CONFIG=Release
 
+# Enable building shared libraries
+# Disable the Fortan interface
+#   - By default it is enabled, which requires the meta.yaml.tmpl to include
+#     gfortran_linux-x64 (via anaconda channel).
+#     This was causing an ld cannot find -lgomp error.
 cmake .. \
-    -DBUILD_SHARED_LIBS=true
+    -DBUILD_SHARED_LIBS=TRUE \
+    -DXSDK_ENABLE_Fortran=FALSE \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG%
 
